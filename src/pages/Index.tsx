1e371db8fe9357e80b92824157ea27a4e4635b4f
Aman Gupta <amanmacair98@gmail.com>
2026-04-10 22:31:12 +0530

    feat: add routing pages including Index and NotFound

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import { SimulationCanvas } from '@/components/simulation/SimulationCanvas';
 import { HUD } from '@/components/simulation/HUD';
 import { PotholeOverlay } from '@/components/simulation/PotholeOverlay';
+import { LaneOverlay } from '@/components/simulation/LaneOverlay';
 
 const Index = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-background">
       <SimulationCanvas />
+      <LaneOverlay />
       <PotholeOverlay />
       <HUD />
     </div>
